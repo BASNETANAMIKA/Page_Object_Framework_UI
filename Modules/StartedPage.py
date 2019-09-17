f589ea13cd60255ee3aa4f819d18835a9bf0aa14
BASNETANAMIKA <>
2019-09-17 16:44:23 +0530

    modifying fn for is page displayed

--- Modules/StartedPage.py
+++ Modules/StartedPage.py
@@ -14,23 +14,18 @@ class StartedPage(PageObject):
         PageObject.__init__(self)
         self.utils = Utils()
 
-    # def _is_current_page(self):
-    #     # this site uses the same title for many pages,
-    #     # so we can't rely on the default implementation
-    #     # of this function. Instead, we'll check the page
-    #     # location, and raise an appropriate error if
-    #     # we are not on the correct page
-    #     location = self.selib.get_location()
-    #     log.info("location is {0}".format(location))
-    #     if not (self.PAGE_URL in location):
-    #         message = "Expected location to end with " + \
-    #                   self.PAGE_URL + " but it did not"
-    #         raise Exception(message)
-    #     return True
-
     def _is_current_page(self):
-        actual_title = self.selib.get_title()
-        log.info(actual_title)
+        # this site uses the same title for many pages,
+        # so we can't rely on the default implementation
+        # of this function. Instead, we'll check the page
+        # location, and raise an appropriate error if
+        # we are not on the correct page
+        location = self.selib.get_location()
+        log.info("location is {0}".format(location))
+        if not (self.PAGE_URL in location):
+            message = "Expected location to end with " + \
+                      self.PAGE_URL + " but it did not"
+            raise Exception(message)
         return True
 
     def verify_started_page(self):
