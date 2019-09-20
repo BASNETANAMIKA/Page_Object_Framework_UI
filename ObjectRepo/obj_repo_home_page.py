d87468392fdadaf466cbe4e814ee5bb4c80fd4b5
BASNETANAMIKA <>
2019-09-20 10:46:13 +0530

    Log Files generated

--- ObjectRepo/obj_repo_home_page.py
+++
@@ -1,6 +0,0 @@
-selectors: {
-#     "build_dropdown" :"xpath=//a[contains(text(),'Build')]",
-    "build_dropdown" :"css=li.has-submenu.is-dropdown-submenu-parent.opens-left:nth-child(2) > a:nth-child(1)",
-    "logo" :"xpath=//div[@class='top-bar-left']//a//img",
-    "getting_started_link" : "xpath=//a[contains(text(),'Getting Started')]"
-}
